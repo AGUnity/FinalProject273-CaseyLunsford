8b6ae104a5df879a733da6687ca07e9ce9b7f7f4
CLUnity <44986121+CLUnity@users.noreply.github.com>
2018-11-19 02:09:22 -0500

    NewAnimationForKeepingHoldLevel
    Made some new triggers to go with the levels and will add the rest of the base design through the 19th.

---
+++ FinalProject273-CaseyLunsford/Assets/Scripts/EOLElevatorTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EOLElevatorTrigger : MonoBehaviour {
+
+    public Animator TargetAnimator = null;
+    public string TriggerName = string.Empty;
+
+    void OnTriggerEnter(Collider Col)
+    {
+        TargetAnimator.SetTrigger(TriggerName);
+    }
+}
---
+++ FinalProject273-CaseyLunsford/Assets/Scripts/LoadUIEndOfKeepingHold.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadUIEndOfKeepingHold : MonoBehaviour {
+
+    public string TriggerName = string.Empty;
+
+    void OnTriggerEnter(Collider Col)
+    {
+        SceneManager.LoadScene(0);
+    }
+}
