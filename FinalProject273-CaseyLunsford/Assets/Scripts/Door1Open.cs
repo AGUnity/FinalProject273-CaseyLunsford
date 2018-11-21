afe04e7ce66f8ffdd94c64d664b998e631cceb9c
CLUnity <44986121+CLUnity@users.noreply.github.com>
2018-11-20 23:08:50 -0500

    Room1AlmostDone
    Room 1 of Level 2/2 is almost done going to be working more on it tommorow to finish room 1 and 2 and 3 if time is available.

---
+++ FinalProject273-CaseyLunsford/Assets/Scripts/Door1Open.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door1Open : MonoBehaviour {
+
+    public Animator TargetAnimator = null;
+    public string TriggerName1 = string.Empty;
+
+    void OnTriggerEnter(Collider Col)
+    {
+        TargetAnimator.SetTrigger(TriggerName1);
+    }
+}
