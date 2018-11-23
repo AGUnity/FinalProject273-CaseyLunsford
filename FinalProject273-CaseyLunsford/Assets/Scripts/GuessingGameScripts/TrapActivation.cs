0e22c6d24b3780f24920387e3590ff4a869b6b89
CLUnity <44986121+CLUnity@users.noreply.github.com>
2018-11-22 19:05:51 -0500

    Level2Changed
    More work added to level two slight difference of choice but for the better. Nearing completion minus desgin which will be taken care of after level desgin is finished.

---
+++ FinalProject273-CaseyLunsford/Assets/Scripts/GuessingGameScripts/TrapActivation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapActivation : MonoBehaviour {
+
+    public GameObject Object1;
+    public GameObject Object2;
+    private void OnTriggerEnter()
+    {
+        Destroy(Object1);
+        Destroy(Object2);
+    }
+}
---
+++ FinalProject273-CaseyLunsford/Assets/Scripts/ReloadGuessingGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReloadGuessingGame : MonoBehaviour {
+
+    public void OnTriggerEnter()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
