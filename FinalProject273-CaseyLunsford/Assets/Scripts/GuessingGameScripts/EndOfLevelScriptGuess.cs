d8dc0e4a5eeef2b4b4aeb8219f2bac52e5114444
CLUnity <44986121+CLUnity@users.noreply.github.com>
2018-11-23 20:44:00 -0500

    EndMainLevelBuilding
    Animations and Levels are built just missing designs and will be done afterwards.

---
+++ FinalProject273-CaseyLunsford/Assets/Scripts/GuessingGameScripts/EndOfLevelScriptGuess.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndOfLevelScriptGuess : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        print("Congrats you have finished this game!");
+    }
+}
--- FinalProject273-CaseyLunsford/Assets/Scripts/GuessingGameScripts/ReloadTrapAttack.cs
+++ FinalProject273-CaseyLunsford/Assets/Scripts/GuessingGameScripts/ReloadTrapAttack.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ReloadGuessingGame : MonoBehaviour {
+public class ReloadTrapAttack : MonoBehaviour {
 
     public void OnTriggerEnter()
     {
